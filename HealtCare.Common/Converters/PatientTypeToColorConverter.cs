ad45a4bb513b114082c24872df4adddf840eb6c0
erkantaylan <etaylan13@gmail.com>
2017-09-28 16:50:04 +0300

    added converters

---
+++ HealtCare.Common/Converters/PatientTypeToColorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HealtCare.Common.Converters {
+
+    public class PatientTypeToColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ HealtCare.Common/Converters/PatientTypeToHumanizeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HealtCare.Common.Converters {
+
+    public class PatientTypeToHumanizeConverter : IValueConverter {
+        private readonly Dictionary<string, string> patientTypes = new Dictionary<string, string> {
+            {
+                "Pregnant", "HAMİLE"
+            }, {
+                "Child", "5 YAŞ ALTI"
+            }, {
+                "Disabled", "ENGELLİ"
+            }, {
+                "Old", "65 YAŞ ÜSTÜ"
+            }, {
+                "Normal", "Normal"
+            }, {
+                "Visitor", "ZİYARETÇİ"
+            }
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string o = value.ToString();
+            return patientTypes[o];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
+}
